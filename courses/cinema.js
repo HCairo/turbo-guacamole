5ee86c8b9746cc59b2a6c2b9997b705c7ba8d809
Duraygue <hugo.cairoh@gmail.com>
2023-11-28 16:12:31 +0100

    COURS DE JS

---
+++ courses/cinema.js
@@ -0,0 +1,11 @@
+const ticket = {
+    nomFilm : "Oppenheimer",
+    prix : 10,
+    numeroSalle : "8"
+}
+
+let nom = "Hugo"
+
+let texteAffichage = "Bonjour " + nom + ", votre film " + ticket.nomFilm + " est en salle " + ticket.numeroSalle
+
+console.log(texteAffichage)
---
+++ courses/tablo.js
@@ -0,0 +1,13 @@
+let playlist = ["Mauvais Payeur", "Saboteur", "Mac11"]
+totalMorceaux = 3
+
+playlist.push("Moi, je prouve")
+playlist.push("Wesh alors !")
+
+totalMorceaux = 5
+
+playlist.pop()
+
+totalMorceaux = 4
+
+console.log(playlist)
