65b026474511b6a1924f9c2c0110054340a7fc62
Duraygue <hugo.cairoh@gmail.com>
2023-11-28 14:19:43 +0100

    BIG UPDATE

--- home.php
+++ home.php
@@ -3,23 +3,40 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Turbo-Guacamole</title>
+        <title>Shnakrs</title>
         <link rel="stylesheet" href="main.css">
+        <link rel="apple-touch-icon" sizes="57x57" href="favicon/apple-icon-57x57.png">
+        <link rel="apple-touch-icon" sizes="60x60" href="favicon/apple-icon-60x60.png">
+        <link rel="apple-touch-icon" sizes="72x72" href="favicon/apple-icon-72x72.png">
+        <link rel="apple-touch-icon" sizes="76x76" href="favicon/apple-icon-76x76.png">
+        <link rel="apple-touch-icon" sizes="114x114" href="favicon/apple-icon-114x114.png">
+        <link rel="apple-touch-icon" sizes="120x120" href="favicon/apple-icon-120x120.png">
+        <link rel="apple-touch-icon" sizes="144x144" href="favicon/apple-icon-144x144.png">
+        <link rel="apple-touch-icon" sizes="152x152" href="favicon/apple-icon-152x152.png">
+        <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-icon-180x180.png">
+        <link rel="icon" type="image/png" sizes="192x192"  href="favicon/android-icon-192x192.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="96x96" href="favicon/favicon-96x96.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
+        <link rel="manifest" href="favicon/manifest.json">
+        <meta name="msapplication-TileColor" content="#ffffff">
+        <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+        <meta name="theme-color" content="#ffffff">
     </head>
     <body>
         <header>
             <nav id="menu">
                 <a href="#">Accueil</a>
-                <a href="#">Gallerie</a>
-                <a href="#"><img src="img/ZCvptdMUvrVrUV9MU8Xo--1--x9396.jpg" alt="Logo"></a>
-                <a href="#">Contact</a>
-                <a href="#">Oskour</a>
+                <a href="#">Actualités</a>
+                <a href="#"><img src="img/logo.png" alt="Logo"></a>
+                <a href="#">Messagerie</a>
+                <a href="#">Profil</a>
             </nav>
         </header>
         
         <section>
-            <h1>Bienvenue sur Turbo-Guacamole</h1>
-            <div class="mimg"><img src="img/banner.webp" alt=""></div>
+            <h1>Bienvenue sur Shnakrs</h1>
+            <p>Le meilleur endroit pour partager tes outfits et discuter avec des personnes qui ont les mêmes intérêts que toi.</p>
         </section>
     </body>
 </html>
